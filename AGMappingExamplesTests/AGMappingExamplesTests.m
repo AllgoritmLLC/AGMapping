bfc517ecad6bdafaf78a53fb74051d75d29b0ef3
v.bezugly <valnocorner@gmail.com>
2015-02-04 14:29:19 +0300

    examples project

---
+++ AGMappingExamplesTests/AGMappingExamplesTests.m
@@ -0,0 +1,40 @@
+//
+//  AGMappingExamplesTests.m
+//  AGMappingExamplesTests
+//
+//  Created by develop on 04/02/15.
+//  Copyright (c) 2015 Allgoritm LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface AGMappingExamplesTests : XCTestCase
+
+@end
+
+@implementation AGMappingExamplesTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
